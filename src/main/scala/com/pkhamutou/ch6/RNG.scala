152b3cf1095e80265589c5b0464aa5074c92c63a
Pavel Khamutou <p.khamutou@gmail.com>
2017-06-15 15:43:54 +0200

    State

--- src/main/scala/com/pkhamutou/ch6/RNG.scala
+++ src/main/scala/com/pkhamutou/ch6/RNG.scala
@@ -54,4 +54,80 @@ object RNG {
     }
     loop(count, rng, Nil)
   }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  val int: Rand[Int] = _.nextInt
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+    val (a, rng2) = s(rng)
+    (f(a), rng2)
+  }
+
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+
+  val doubleMap: Rand[Double] = map(nonNegativeInt)(d => d / (Int.MaxValue.toDouble + 1))
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    val (a, sa) = ra(rng)
+    val (b, sb) = rb(sa)
+    (f(a, b), sb)
+  }
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] = map2(ra, rb)((_, _))
+
+  val randIntDouble: Rand[(Int, Double)] = both(int, double)
+  val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List.empty[A]))((a, z) => map2(a, z)(_ :: _))
+
+  def intsSequence(count: Int): Rand[List[Int]] = sequence(List.fill(count)(int))
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (r, s) = f(rng)
+    g(r)(s)
+  }
+
+  def nonNegativeLessThen(n: Int): Rand[Int] = flatMap(nonNegativeInt) { i =>
+    val mod = i % n
+    if (i + (n - 1) - mod >= 0) unit(mod) else nonNegativeLessThen(n)
+  }
+
+  def _map[A, B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(x => unit(f(x)))
+
+  def _map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = flatMap(ra)(a => map(rb)(b => f(a, b)))
+
+  def rollDie: Rand[Int] = _map(nonNegativeLessThen(6))(_ + 1)
+}
+
+case class State[S, +A](run: S => (A, S)) {
+  import State._
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s2) = run(s)
+    f(a).run(s2)
+  })
+
+  def map[B](f: A => B): State[S, B] = flatMap(a => unit(f(a)))
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = flatMap(a => sb.map(b => f(a, b)))
+}
+
+object State {
+  type Rand[A] = State[RNG, A]
+
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+
+  def sequence[S, A](xs: List[State[S, A]]): State[S, List[A]] =
+    xs.foldRight(unit[S, List[A]](List.empty[A]))((a, z) => a.map2(z)(_ :: _))
+
+  val int: Rand[Int] = State(_.nextInt)
+
+  def get[S]: State[S, S] = State(s => (s, s))
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  def modify[S](f: S => S): State[S, Unit] = get.flatMap(s => set(f(s)))
+
 }
--- src/test/scala/com/pkhamutou/ch6/RNGTest.scala
+++ src/test/scala/com/pkhamutou/ch6/RNGTest.scala
@@ -1,5 +1,6 @@
 package com.pkhamutou.ch6
 
+import com.pkhamutou.ch6.RNG.Rand
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FunSuite, Matchers}
 
@@ -51,6 +52,27 @@ class RNGTest extends FunSuite with Matchers with GeneratorDrivenPropertyChecks 
       val ints = RNG.ints(3)(rng)
       ints._1 should have size 3
     }
+  }
 
+  test("testDoubleMap") {
+    forAll { seed: Long =>
+      val rng = RNG.Simple(seed)
+      RNG.doubleMap(rng)._1 should (be >= 0d and be < 1d)
+    }
+  }
+
+  test("testSequence") {
+    forAll { seed: Long =>
+      val rng = RNG.Simple(seed)
+      val xs: List[Rand[Int]] = List.fill(2)(RNG.int)
+      RNG.sequence(xs)(rng)._1 should have size 2
+      RNG.intsSequence(3)(rng)._1 should have size 3
+    }
+  }
+
+  test("testRollDie") {
+    forAll { seed: Long =>
+      RNG.rollDie(RNG.Simple(seed))._1 should (be >= 1 and be <= 6)
+    }
   }
 }
